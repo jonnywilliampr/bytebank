924931ab9e21e70f0fc888782eaaf493044871f2
Jonny William <jonnywilliampr@gmail.com>
2021-08-24 20:41:26 -0300

    Adicionando construction para simplificar o código.

--- js/Cliente.js
+++ js/Cliente.js
@@ -1,4 +1,13 @@
 export class Cliente{ //a classe cliente é um molde
     nome;
-    cpf;
+    _cpf;
+
+    get cpf(){
+        return this._cpf;
+    }
+
+    constructor(nome, cpf){
+        this.nome = nome;
+        this._cpf = cpf;
+    }
 }
--- js/index.js
+++ js/index.js
@@ -1,25 +1,16 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 
-const cliente1 = new Cliente(); //cliente 1 e cliente 2 são uns objetos
-const cliente2 = new Cliente(); //estou pedindo para a partir do molde criar novos objetos
-const contaCorrente1 = new ContaCorrente();
-const contaCorrente2 = new ContaCorrente();
+const cliente1 = new Cliente("Jonny", 10263693473); //cliente 1 e cliente 2 são uns objetos
 
-cliente1.nome = "Jonny";
-cliente1.cpf = 10263693473;
-contaCorrente1.agencia = 1001;
-contaCorrente1._saldo = 0;
+const cliente2 = new Cliente("Neto", 10738348596); //estou pedindo para a partir do molde criar novos objetos
 
-cliente2.nome = "Neto";
-cliente2.cpf = 10738348596;
-contaCorrente2.agencia = 1002;
-contaCorrente2._saldo = 100;
+const contaCorrente1 = new ContaCorrente(1001, cliente1);
+contaCorrente1.depositar(500);
 
-contaCorrente1.depositar(200);
+const contaCorrente2 = new ContaCorrente(1002, cliente2);
 
-const valorSacado = contaCorrente1.sacar(50);
+let valor = 200;
+contaCorrente1.transferir(valor, contaCorrente2);
 
-console.log(valorSacado);
-
-console.log(contaCorrente1);
+console.log(ContaCorrente.numeroDeContas);
